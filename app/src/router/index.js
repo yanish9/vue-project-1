f11eae781f9ffbe92f2c0ffe168748e4a031878b
yanish9 <yanishguru@gmail.com>
2020-11-28 15:48:34 -0500

    notes v1

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -9,10 +9,18 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
+  }, {
+    path: '/',
+    name: 'login',
+    component: Home
+  }, {
+    path: '/',
+    name: 'mynotes',
+    component: Home
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/settings',
+    name: 'About', 
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/img/logo.svg">
+    <Home msg="Welcome to My Notes App"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Home from '@/components/Home.vue'
 
 export default {
-  name: 'Home',
+  name: 'home',
   components: {
-    HelloWorld
+    Home
   }
 }
 </script>
