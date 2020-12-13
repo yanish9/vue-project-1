8c703712537ec27e2cc6e9c72547887f06b10618
yanish9 <yanishguru@gmail.com>
2020-12-13 15:01:03 -0500

    login

---
+++ app/src/views/login/login.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img width="200" alt="Vue logo" src="../../assets/img/logo.svg">
+  <Login ></Login>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Login from '@/components/login/login.vue'
+
+export default {
+  name: 'login',
+  components: {
+    Login
+  }
+}
+</script>
